f5811d2b6b41e5841e2a36aabac8fc9f1017aa4a
BrunoYamamoto2204 <brunoeiky2006@gmail.com>
2025-08-21 15:28:13 -0300

    Mentoria e Curso

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,29 @@
-package DesafioPOO.src.classes;
+package DesafioPOO.src;
+
+import DesafioPOO.src.classes.Cursos;
+import DesafioPOO.src.classes.Mentoria;
+
+import java.time.LocalDate;
 
 public class Main {
+    public static void main(String[] args) {
+        Cursos curso1 = new Cursos();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso Java");
+        curso1.setCargaHoraria(8);
+
+        Cursos curso2 = new Cursos();
+        curso2.setTitulo("Curso JS");
+        curso2.setDescricao("Descrição curso JS");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria Java");
+        mentoria.setDescricao("Descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
 }
